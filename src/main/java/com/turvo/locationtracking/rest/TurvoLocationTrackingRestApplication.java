582cd281a603180b0af76bd0d4fa562320a43a61
pavankiranmaddela <pavankiran.maddela@gmail.com>
2017-11-03 19:15:56 +0530

    First commit

---
+++ src/main/java/com/turvo/locationtracking/rest/TurvoLocationTrackingRestApplication.java
@@ -0,0 +1,12 @@
+package com.turvo.locationtracking.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurvoLocationTrackingRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurvoLocationTrackingRestApplication.class, args);
+	}
+}
